7350ebb00d9aae014bb0053361a447e84e3b69e2
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2016-12-18 15:54:49 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Stopping a programme from crashing.
+using System;
+class TryIt
+{
+	public static void Main()
+	{
+		//dataType array, arrayName, =new dataType array of 5.
+		int[] mikesArray = new int[5];
+		
+		try //start of error testing.
+		{
+			//loops ctr 10x
+			for (int ctr=0; ctr<10; ctr++) 
+			{
+				//array counter is asigned to ctr.
+				mikesArray[ctr] = ctr;
+			}
+		}
+		catch(Exception e)  //testing ends and error is caught.
+		{
+			Console.WriteLine("The exception was caught Mike: {0}", e);
+		}
+		finally
+		{
+			Console.WriteLine("Last thing to run.");
+		}
+		Console.ReadKey(true);
+	}
+}
